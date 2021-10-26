96519e2ca852722d7b51b85d4a17011c3b0440c0
Daniel Castillo <danny.castle@hotmail.com>
2021-10-25 22:40:04 -0400

    started layout

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
+const operate = (operator, a, b) => {
+    let answer;
+    switch (operator){
+        case '+':
+            answer = add(a,b);
+            break;
+
+        case '-':
+            answer = subtract(a,b);
+            break;
+
+        case '*':
+            answer = multiply(a,b);
+            break;
+
+        case '/':
+            answer = divide(a,b);
+            break;
+
+        case '^':
+            answer = power(a,b);
+            break;
+
+        case '!':
+            answer = factorial(a);
+            break;
+
+
+    };
+    return answer;
+};
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
 
-const multiply = function(arr) {
-    let total = arr.reduce((total, currentValue) =>{
-        return total *= currentValue;
-    });
- return total;
-};
+const multiply = (a,b) => a * b;
 
-const power = function(a,b) {
+const divide = (a,b) => a / b;
+
+const power = (a,b) => {
     let i = 2;
     let product = a;
     while(b >= i){
@@ -20,7 +49,7 @@ const power = function(a,b) {
 
 };
 
-const factorial = function(a) {
+const factorial = (a) => {
     let product = a;
     while(a > 1){
         product *= a - 1;
